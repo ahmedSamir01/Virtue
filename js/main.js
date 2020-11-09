ca9203937c8b757e7ae40bd5b1f61b80cba86bfa
ahmed samir <asproducer80@gmail.com>
2020-11-09 17:20:46 +0200

    editnav

--- js/main.js
+++ js/main.js
@@ -109,5 +109,10 @@ $(function () {
         
     });
     
+    // close when click somewhere
+    $(document).bind("click", function(e) {
+        if ($(".navbar-collapse").hasClass("show") && e.target !== document.querySelector(".show") && e.target !== document.querySelector(".navbar-toggler")) {$(".navbar-toggler").click()}
+    });
+    
 });
 
